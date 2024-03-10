94a90bda1abc3aa01eef6e0b78f99584a00041e1
Aaxhirrr <anola133@asu.edu>
2024-03-10 11:29:36 -0700

    Add model.summary() printout to notebook

--- .ghost_heart.py
+++ .ghost_heart.py
@@ -142,3 +142,4 @@
 # Set optimizer to Adam with custom learning rate
 # Document all hyperparameters in comments
 # Set optimizer to Adam with custom learning rate
+# Add model.summary() printout to notebook
